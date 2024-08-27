94c498e975934775a43e8da70075f0329dff09ab
Allen Zhang (张涛) <wr_zhang25@163.com>
2024-08-27 15:27:42 +0800

    upd

--- templates/write-canyon-to-local-template.js
+++ templates/write-canyon-to-local-template.js
@@ -1,26 +1,30 @@
-if (window.isDocumentAddVisibilitychangeWriteCanyonToLocal) {
-} else {
-  window.isDocumentAddVisibilitychangeWriteCanyonToLocal = true
-  if (window.document) {
-    if (window.writeCanyonToLocal) {
-      window.manualWriteCanyonToLocal = function () {
-        window.writeCanyonToLocal(JSON.stringify({
-          coverage: window.__coverage__,
-          canyon: window.__canyon__
-        }))
-      }
-    }
-    document.addEventListener('visibilitychange', function () {
-      if (document.visibilityState === 'hidden') {
-        if (window.writeCanyonToLocal) {
+try {
+  if (window.isDocumentAddVisibilitychangeWriteCanyonToLocal) {
+  } else {
+    window.isDocumentAddVisibilitychangeWriteCanyonToLocal = true
+    if (window.document) {
+      if (window.writeCanyonToLocal) {
+        window.manualWriteCanyonToLocal = function () {
           window.writeCanyonToLocal(JSON.stringify({
             coverage: window.__coverage__,
             canyon: window.__canyon__
           }))
-        } else {
-          console.log('no writeCanyonToLocal function')
         }
       }
-    });
+      document.addEventListener('visibilitychange', function () {
+        if (document.visibilityState === 'hidden') {
+          if (window.writeCanyonToLocal) {
+            window.writeCanyonToLocal(JSON.stringify({
+              coverage: window.__coverage__,
+              canyon: window.__canyon__
+            }))
+          } else {
+            console.log('no writeCanyonToLocal function')
+          }
+        }
+      });
+    }
   }
+} catch (e) {
+  
 }
